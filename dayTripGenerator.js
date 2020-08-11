277db49459534f53b6bec503a86da6f0e82c0bee
cwadeking <charles.w.king.cwk@gmail.com>
2020-08-11 18:54:38 -0500

    started working on refactoring.  created function for changing a specific part of the trip and working on a displaySelectedTrip function

--- dayTripGenerator.js
+++ dayTripGenerator.js
@@ -14,7 +14,13 @@ function pickRandomIndexFromArray(arrayPassedIn,randomIndexNumber){
 }
 
 function runProgram(){
+   let randomDayTrip = randomDayTrip();
+
+}
+
+function randomDayTrip{
     let responseToContinue;
+    let randomDayTripArray;
     while(!responseToContinue){
         
         let citiesArray = ["Madison", "Port Washington", "Kenosha", "Green Bay", "Milwaukee"];
@@ -29,7 +35,39 @@ function runProgram(){
         let usersDayTrip = `Have fun going to ${cityPicked}, eating at ${restaurantPicked}, getting there by 
         ${modeOfTransportationPicked}, and being entertained by ${entertainmentPicked}`;
         console.log(usersDayTrip);
+        randomDayTripArray = [cityPicked, restaurantPicked,modeOfTransportationPicked,entertainmentPicked];
         let checkResponse = prompt("Would you like to select another day trip? Please type, yes or no");
         responseToContinue = checkResponse == "yes" ? null : true;
+        let askToModifyTrip = prompt("Would you like to keep some parts of this trip and change some others?")
+        if(askToModifyTrip === "yes"){
+            return customTrip(randomDayTripArray, citiesArray, restaurantsArray, modeOfTransportation, formOfEntertainment)
+        } 
+
+    }
+}
+function displayTrip(selectedTripArray){
+    
+}
+
+function customTrip(randomUserArray, citiesArray, restaurantsArray, transportationArray, entertainmentArray){
+    let valueToChange;
+    alert("What would you like to change about the random trip generated for you?");
+    while(valueToChange !== ("1" || "2" || "3" || "4")){
+        valueToChange = prompt(`Would you like to change 1:${randomUserArray[0]}, 2:${randomUserArray[1]}, 3:${randomUserArray[2]},
+        or 4:${randomUserArray[3]}`)
+    }
+    switch(valueToChange){
+        case "1":
+            randomUserArray[0] = pickRandomIndexFromArray(citiesArray, pickRandomNumberBasedOnArraySize(citiesArray));
+            break;
+        case "2":
+            randomUserArray[1] = pickRandomIndexFromArray(restaurantsArray, pickRandomNumberBasedOnArraySize(restaurantsArray));
+            break;
+        case "3":
+            randomUserArray[2] = pickRandomIndexFromArray(transportationArray, pickRandomNumberBasedOnArraySize(transportationArray));
+            break;
+        case "4":
+            randomUserArray[3] = pickRandomIndexFromArray(entertainmentArray, pickRandomNumberBasedOnArraySize(entertainmentArray));
+            break;
     }
 }
