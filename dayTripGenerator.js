128c885c752a75ce34a092564a55fcb29203a363
cwadeking <charles.w.king.cwk@gmail.com>
2020-08-10 21:53:44 -0500

    fixed random number generator and tested methods.  Going to test more, if nothing needed, will submit

--- dayTripGenerator.js
+++ dayTripGenerator.js
@@ -1,13 +1,17 @@
 "use strict"
+let citiesArray = [];
+let restaurantsArray = [];
+let modeOfTransportation = [];
+let formOfEntertainment = [];
 
-let citiesArray = ["Madison", "Port Washington", "Kenosha", "Green Bay", "Milwaukee"];
-let restaurantsArray = ["Steak and Shake", "Five O Clock", "Chili's", "Public Market"];
-let modeOfTransportation = ["Beat feet", "Scooter", "Car", "Hot Air Balloon"];
-let formOfEntertainment = ["Movie", "Football Game", "Roller Blading", "Downtown Tour"];
+
+runProgram();
+
+
 
 function pickRandomNumberBasedOnArraySize(arrayToFindRandomValue){
     let selectionFromArray;
-    selectionFromArray = Math.floor(arrayToFindRandomValue.length) + 1;
+    selectionFromArray = Math.floor(Math.random() * arrayToFindRandomValue.length);
     return selectionFromArray;
 }
 
@@ -17,17 +21,22 @@ function pickRandomIndexFromArray(arrayPassedIn,randomIndexNumber){
 }
 
 function runProgram(){
-    let responseTocontinue;
+    let responseToContinue;
     while(!responseToContinue){
-        let cityNumber = pickRandomNumberBasedOnArraySize(citiesArray);
-        let cityPicked = pickRandomIndexFromArray(citiesArray, cityNumber);
-        let restaurantNumber = pickRandomIndexFromArray(restaurantArray);
-        let restaurantPicked = pickRandomIndexFromArray(restaurant, restaurantNumber);
-        let modeOfTransportationNumber = pickRandomIndexFromArray(modeOfTransportation);
-        let modeOfTransportationPicked = pickRandomIndexFromArray(modeOfTransportation, modeOfTransportationNumber);
-        let entertainmentNumber = pickRandomIndexFromArray(formOfEntertainment);
-        let entertainmentPicked = pickRandomIndexFromArray(formOfEntertainment, entertainmentNumber);
-        let checkResponse = prompt("Would you like to select another day trip?")
-        let responseToContinue = c
+        
+        citiesArray = ["Madison", "Port Washington", "Kenosha", "Green Bay", "Milwaukee"];
+        let cityPicked = pickRandomIndexFromArray(citiesArray, pickRandomNumberBasedOnArraySize(citiesArray));
+        restaurantsArray = ["Steak and Shake", "Five O Clock", "Chili's", "Public Market"];
+        let restaurantPicked = pickRandomIndexFromArray(restaurantsArray, pickRandomNumberBasedOnArraySize(restaurantsArray));
+        modeOfTransportation = ["Beat feet", "Scooter", "Car", "Hot Air Balloon"];
+        let modeOfTransportationPicked = pickRandomIndexFromArray(modeOfTransportation, pickRandomNumberBasedOnArraySize(modeOfTransportation));
+        formOfEntertainment = ["Movie", "Football Game", "Roller Blading", "Downtown Tour"];
+        let entertainmentPicked = pickRandomIndexFromArray(formOfEntertainment, pickRandomNumberBasedOnArraySize(formOfEntertainment));
+  
+        let usersDayTrip = `Have fun going to ${cityPicked}, eating at ${restaurantPicked}, getting there by 
+        ${modeOfTransportationPicked}, and being entertained by ${entertainmentPicked}`;
+        console.log(usersDayTrip);
+        let checkResponse = prompt("Would you like to select another day trip? Please type, yes or no");
+        responseToContinue = checkResponse == "yes" ? null : true;
     }
 }
