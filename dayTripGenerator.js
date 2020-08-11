cc7a675222e8910bf850aeb6b256f9b045676bc2
cwadeking <charles.w.king.cwk@gmail.com>
2020-08-10 20:36:32 -0500

    created function to take in any array and random number so that an index can be extracted.

--- dayTripGenerator.js
+++ dayTripGenerator.js
@@ -5,8 +5,29 @@ let restaurantsArray = ["Steak and Shake", "Five O Clock", "Chili's", "Public Ma
 let modeOfTransportation = ["Beat feet", "Scooter", "Car", "Hot Air Balloon"];
 let formOfEntertainment = ["Movie", "Football Game", "Roller Blading", "Downtown Tour"];
 
-function pickRandomValueInArray(arrayToFindRandomValue){
+function pickRandomNumberBasedOnArraySize(arrayToFindRandomValue){
     let selectionFromArray;
     selectionFromArray = Math.floor(arrayToFindRandomValue.length) + 1;
     return selectionFromArray;
+}
+
+function pickRandomIndexFromArray(arrayPassedIn,randomIndexNumber){
+    let randomSelection = arrayPassedIn[randomIndexNumber];
+    return randomSelection;        
+}
+
+function runProgram(){
+    let responseTocontinue;
+    while(!responseToContinue){
+        let cityNumber = pickRandomNumberBasedOnArraySize(citiesArray);
+        let cityPicked = pickRandomIndexFromArray(citiesArray, cityNumber);
+        let restaurantNumber = pickRandomIndexFromArray(restaurantArray);
+        let restaurantPicked = pickRandomIndexFromArray(restaurant, restaurantNumber);
+        let modeOfTransportationNumber = pickRandomIndexFromArray(modeOfTransportation);
+        let modeOfTransportationPicked = pickRandomIndexFromArray(modeOfTransportation, modeOfTransportationNumber);
+        let entertainmentNumber = pickRandomIndexFromArray(formOfEntertainment);
+        let entertainmentPicked = pickRandomIndexFromArray(formOfEntertainment, entertainmentNumber);
+        let checkResponse = prompt("Would you like to select another day trip?")
+        let responseToContinue = 
+    }
 }
