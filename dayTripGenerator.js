829d96c1bf69a2f33e7f80ab65009b10ed9f0687
cwadeking <charles.w.king.cwk@gmail.com>
2020-08-11 20:20:47 -0500

    Created the custom trip, display trip, and tested.  Creating one more function to make a decision for the trip generator program

--- dayTripGenerator.js
+++ dayTripGenerator.js
@@ -14,11 +14,11 @@ function pickRandomIndexFromArray(arrayPassedIn,randomIndexNumber){
 }
 
 function runProgram(){
-   let randomDayTrip = randomDayTrip();
+    randomDayTrip();
 
 }
 
-function randomDayTrip{
+function randomDayTrip(){
     let responseToContinue;
     let randomDayTripArray;
     while(!responseToContinue){
@@ -34,11 +34,12 @@ function randomDayTrip{
   
        
         randomDayTripArray = [cityPicked, restaurantPicked,modeOfTransportationPicked,entertainmentPicked];
+        displayTrip(randomDayTripArray);
         let checkResponse = prompt("Would you like to select another random day trip? Please type, yes or no");
         if(checkResponse !== "yes"){
             let askAboutCustomTrip = prompt(`Would you like to keep some parts of this trip and change some others?`);
             if(askAboutCustomTrip === "yes"){
-                return customTrip(randomDayTripArray, citiesArray, restaurantsArray, modeOfTransportation, formOfEntertainment));
+                return customTrip(randomDayTripArray, citiesArray, restaurantsArray, modeOfTransportation, formOfEntertainment);
             }
         }
         responseToContinue = checkResponse == "yes" ? null : true;
@@ -47,9 +48,13 @@ function randomDayTrip{
 
     }
 }
+function makeChoiceAboutTripGenerator(){}
 function displayTrip(selectedTripArray){
-    let usersDayTrip = `Have fun going to ${cityPicked}, eating at ${restaurantPicked}, getting there by 
-    ${modeOfTransportationPicked}, and being entertained by ${entertainmentPicked}`;
+    let usersDayTrip = `Have fun going to ${selectedTripArray[0]}, eating at ${selectedTripArray[1]}, getting there by 
+    ${selectedTripArray[2]}, and being entertained by ${selectedTripArray[3]}`;
+    let randomOrCustom = prompt(`Would you like to: 1: Generate another random trip
+                                                    2: Customize this current trip 
+                                                    3: Exit the program   `)
     alert(usersDayTrip);
 
 }
@@ -64,7 +69,7 @@ function displayCustomTrip(selectedTripArray){
 function customTrip(randomUserArray, citiesArray, restaurantsArray, transportationArray, entertainmentArray){
     let valueToChange;
     alert("What would you like to change about the random trip generated for you?");
-    while(valueToChange !== ("1" || "2" || "3" || "4")){
+    while(valueToChange !== "1" && valueToChange !== "2" && valueToChange !==  "3" && valueToChange !==  "4"){
         valueToChange = prompt(`Would you like to change 1:${randomUserArray[0]}, 2:${randomUserArray[1]}, 3:${randomUserArray[2]},
         or 4:${randomUserArray[3]}`)
     }
